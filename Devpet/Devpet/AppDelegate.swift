c2c145c3afb79f77913ed028126941bfb86231db
KangMinseok <qbq5000@gmail.com>
2020-10-07 14:53:30 +0900

    :tada: Initial Commit

---
+++ Devpet/Devpet/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Devpet
+//
+//  Created by 강민석 on 2020/10/07.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Devpet/Devpet/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Devpet
+//
+//  Created by 강민석 on 2020/10/07.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
