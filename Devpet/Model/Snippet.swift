d98acae5ed642b476d25bcd3d57e0b4f37f4d603
KangMinseok <qbq5000@gmail.com>
2020-12-05 20:42:01 +0900

    🌱 Snippet 모델 수정

--- Devpet/Model/Snippet.swift
+++ Devpet/Model/Snippet.swift
@@ -14,4 +14,12 @@ class Snippet: Object {
     @objc dynamic var snippetDescription = ""
     @objc dynamic var isFavorite = false
     @objc dynamic var data = ""
+    
+    convenience init(name: String, language: String, description: String, data: String) {
+        self.init()
+        self.name = name
+        self.language = language
+        self.snippetDescription = description
+        self.data = data
+    }
 }
