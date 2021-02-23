f5c17c098fb929b49c548134cecaa635d9b17dc4
KangMinseok <qbq5000@gmail.com>
2021-02-24 01:02:14 +0900

    🌱 Devpet SwiftUI 프로젝트 추가

---
+++ SwiftUI/Devpet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Devpet
+//
+//  Created by 강민석 on 2021/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/Devpet/DevpetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevpetApp.swift
+//  Devpet
+//
+//  Created by 강민석 on 2021/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DevpetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
