c94f3c955a4c845bd403ecffffe52b94cfcf8637
KangMinseok <qbq5000@gmail.com>
2021-02-27 00:12:19 +0900

    ✨ 메뉴바 버튼 추가

--- SwiftUI/Devpet/DevpetApp.swift
+++ SwiftUI/Devpet/DevpetApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var popover = NSPopover()
+    var statusBar: StatusBarController?
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        popover.contentSize = NSSize(width: 360, height: 360)
+        popover.contentViewController = NSHostingController(rootView: ContentView())
+        
+        statusBar = StatusBarController(popover)
+    }
+}
 
 @main
 struct DevpetApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
         }
     }
 }
